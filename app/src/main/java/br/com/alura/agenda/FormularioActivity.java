fd76de92a241ffaa28bceb835b8ad0c397bd537b
Fabio Bastos <fabio_basttos@outlook.com>
2017-10-07 00:10:15 -0300

    Salvando o arquivo de imagem

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -2,8 +2,6 @@ package br.com.alura.agenda;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -13,7 +11,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -62,11 +59,7 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == CODIGO_CAMERA) {
-                ImageView foto = (ImageView) findViewById(R.id.formulario_foto);
-                Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
-                Bitmap bitmapReduzido = bitmap.createScaledBitmap(bitmap, 300, 400, true);
-                foto.setImageBitmap(bitmapReduzido);
-                foto.setScaleType(ImageView.ScaleType.FIT_XY);
+                helper.carregaImagem(caminhoFoto);
             }
         }
     }
--- app/src/main/java/br/com/alura/agenda/FormularioHelper.java
+++ app/src/main/java/br/com/alura/agenda/FormularioHelper.java
@@ -1,6 +1,9 @@
 package br.com.alura.agenda;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 
 import br.com.alura.agenda.modelo.Aluno;
@@ -17,6 +20,7 @@ public class FormularioHelper {
     private final EditText campoTelefone;
     private final EditText campoSite;
     private final RatingBar ratingNota;
+    private final ImageView campoFoto;
 
     private Aluno aluno;
 
@@ -26,6 +30,7 @@ public class FormularioHelper {
         campoTelefone = (EditText) activity.findViewById(R.id.formulario_telefone);
         campoSite = (EditText) activity.findViewById(R.id.formulario_site);
         ratingNota = (RatingBar) activity.findViewById(R.id.formulario_nota);
+        campoFoto = (ImageView) activity.findViewById(R.id.formulario_foto);
         aluno = new Aluno();
     }
 
@@ -36,6 +41,7 @@ public class FormularioHelper {
         aluno.setTelefone(campoTelefone.getText().toString());
         aluno.setSite(campoSite.getText().toString());
         aluno.setNota(Double.valueOf(ratingNota.getProgress()));
+        aluno.setCaminhoFoto((String) campoFoto.getTag());
         return aluno;
     }
 
@@ -45,6 +51,17 @@ public class FormularioHelper {
         campoTelefone.setText(aluno.getTelefone());
         campoSite.setText(aluno.getSite());
         ratingNota.setProgress(aluno.getNota().intValue());
+        carregaImagem(aluno.getCaminhoFoto());
         this.aluno = aluno;
+    }
+
+    public void carregaImagem(String caminhoFoto) {
+        if (caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = bitmap.createScaledBitmap(bitmap, 300, 400, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+            campoFoto.setTag(caminhoFoto);//Objeto associal a qualquer view do Android (Ex. ImageView)
+        }
     }
 }
